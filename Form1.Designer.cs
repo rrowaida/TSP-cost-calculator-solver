59914e197486f86a26bb8bc03d665899e3d7fece
Raghad Rowaida <raghad.rowaida101@gmail.com>
2022-07-11 01:49:28 -0400

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace TSP_cost_calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.solveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.graphViewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showCostToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.solveToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(798, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // solveToolStripMenuItem
+            // 
+            this.solveToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.graphViewToolStripMenuItem,
+            this.showCostToolStripMenuItem});
+            this.solveToolStripMenuItem.Name = "solveToolStripMenuItem";
+            this.solveToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.solveToolStripMenuItem.Text = "Solve";
+            // 
+            // graphViewToolStripMenuItem
+            // 
+            this.graphViewToolStripMenuItem.Name = "graphViewToolStripMenuItem";
+            this.graphViewToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.graphViewToolStripMenuItem.Text = "Graph view";
+            this.graphViewToolStripMenuItem.Click += new System.EventHandler(this.graphViewToolStripMenuItem_Click);
+            // 
+            // showCostToolStripMenuItem
+            // 
+            this.showCostToolStripMenuItem.Name = "showCostToolStripMenuItem";
+            this.showCostToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.showCostToolStripMenuItem.Text = "Show cost";
+            this.showCostToolStripMenuItem.Click += new System.EventHandler(this.showCostToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.infoToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(95, 22);
+            this.infoToolStripMenuItem.Text = "Info";
+            this.infoToolStripMenuItem.Click += new System.EventHandler(this.infoToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(573, 136);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(225, 203);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(655, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 27);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "nodes";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "edges";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(567, 312);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(578, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Path Cost";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(655, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Calculation";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 344);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "TSP cost calculator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem solveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem graphViewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem showCostToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;//use to generate chart 
+using System.IO;//use for reading input files
+
+namespace TSP_cost_calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static class GlobalData
+        {
+            public static string contents = "";
+        };
+
+        public static class ListTest
+        {
+            public static List<int> list_ind = new List<int>();
+            public static List<double> list_coorx = new List<double>();
+            public static List<double> list_coory = new List<double>();
+        };
+
+        public bool finding(int[] a, int e, int l)
+        {
+            int f=0;
+            for(int i=0; i<l; i++)
+            {
+                if(a[i]==e)
+                    f=1;
+            }
+
+            if(f==1)
+                return true;
+            else
+                return false;
+        }
+
+        public void init_gen(int[] ar, int l)
+        {
+            int k;
+            bool v;
+
+            Random rand = new Random();
+            
+
+            ar[0] = rand.Next(0, l);
+            for (int i = 1; i < l; i++)
+            {
+                k = rand.Next(0, l);
+
+                v = finding(ar, k, i);
+
+                if (v == true)
+                    i = i - 1;
+                else
+                    ar[i] = k;
+            }
+        }
+
+        public double Eval(double[,] dist, int[] t, int l)
+        {
+            double c = 0;
+
+            for (int i = 0; i < l - 1; i++)
+            {
+                int k = t[i];
+                c = c + dist[t[i],t[i+1]];
+            }
+            c=c+dist[t[l-1],t[0]];
+            return c;
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //chart1.Update();
+            int size = -1;
+            string filename = "";
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                filename = openFileDialog1.FileName;
+                try
+                {
+                    string text = File.ReadAllText(filename);
+                    size = text.Length;
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("unreadable file");
+                }
+
+                GlobalData.contents = File.ReadAllText(@filename);
+            }
+
+            int len = GlobalData.contents.Length;
+
+            string[] words = GlobalData.contents.Split(' ');
+            int i = 0;
+            double[] f_data;
+            f_data = new double[len];
+
+            int v = 0;
+
+            string check = "NODE_COORD_SECTION";
+            richTextBox1.Text = "";
+            GlobalData.contents = "";
+
+            foreach (string word in words)
+            {
+                if (word.Contains(check))
+                    v = 1;
+                if (v == 1)
+                    GlobalData.contents = GlobalData.contents + " " + word;
+            }
+
+            System.Text.RegularExpressions.MatchCollection matches = System.Text.RegularExpressions.Regex.Matches(GlobalData.contents, @"(\d+\.?\d*|\.\d+)");
+
+            string[] MatchList = new string[matches.Count];
+
+            // add each match
+            int c = 0;
+            foreach (System.Text.RegularExpressions.Match match in matches)
+            {
+                MatchList[c] = match.ToString();
+                c++;
+            }
+
+            len = MatchList.Length;
+
+            int value;
+            for (i = 1; i < len; i++)
+            {
+                if (i % 3 == 1)
+                {
+                    int.TryParse(MatchList[i], out value);
+                    ListTest.list_ind.Add(value);
+
+                }
+
+                if (i % 3 == 2)
+                {
+                    ListTest.list_coorx.Add(Convert.ToDouble(MatchList[i]));
+
+                }
+
+                if (i % 3 == 0)
+                {
+                    ListTest.list_coory.Add(Convert.ToDouble(MatchList[i]));
+                }
+            }
+
+            chart1.Show();
+            len = ListTest.list_coorx.Count;
+
+            for (i = 0; i < len; i++)
+            {
+                chart1.Series["nodes"].Points.AddXY(ListTest.list_coorx[i], ListTest.list_coory[i]);
+            }
+            chart1.Series["nodes"].ChartType = SeriesChartType.Point;
+            chart1.Series["nodes"].Color = Color.Red;
+ 
+        }
+
+        private void graphViewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int len = GlobalData.contents.Length;
+
+            string[] words = GlobalData.contents.Split(' ');
+            int i = 0;
+            double[] f_data;
+            f_data = new double[len];
+
+            int v = 0;
+
+            string check = "NODE_COORD_SECTION";
+            richTextBox1.Text = "";
+            GlobalData.contents = "";
+
+            foreach (string word in words)
+            {
+                if (word.Contains(check))
+                    v = 1;
+                if (v == 1)
+                    GlobalData.contents = GlobalData.contents + " " + word;
+            }
+            
+            System.Text.RegularExpressions.MatchCollection matches = System.Text.RegularExpressions.Regex.Matches(GlobalData.contents, @"(\d+\.?\d*|\.\d+)");
+
+            string[] MatchList = new string[matches.Count];
+
+            // add each match
+            int c = 0;
+            foreach (System.Text.RegularExpressions.Match match in matches)
+            {
+                MatchList[c] = match.ToString();
+                c++;
+            }
+
+            len = MatchList.Length;
+            int value;
+            for (i = 1; i < len; i++)
+            {
+                if (i % 3 == 1)
+                {
+                    int.TryParse(MatchList[i], out value);
+                    //GlobalData.ind[j] = value;
+                    ListTest.list_ind.Add(value);
+                    //j++;
+                }
+
+                if (i % 3 == 2)
+                {
+                    //GlobalData.xcoor[k] = Convert.ToDouble(MatchList[i]);
+                    ListTest.list_coorx.Add(Convert.ToDouble(MatchList[i]));
+                    //k++;
+                }
+
+                if (i % 3 == 0)
+                {
+                    //GlobalData.ycoor[l] = Convert.ToDouble(MatchList[i]);
+                    ListTest.list_coory.Add(Convert.ToDouble(MatchList[i]));
+                    //l++;
+                }
+            }
+
+            chart1.Show();
+            len = ListTest.list_coorx.Count;
+            richTextBox1.Text = "";
+            //richTextBox1.Text = richTextBox1.Text + len;
+            for (i = 0; i < len; i++)
+            {
+                //richTextBox1.Text = richTextBox1.Text + ListTest.list_ind[i] + " " + ListTest.list_coorx[i] + " " + ListTest.list_coory[i] + "\n";
+                //richTextBox1.Text = richTextBox1.Text + GlobalData.ind[i] + " " + GlobalData.xcoor[i] + " " + GlobalData.ycoor[i] + "\n";
+            }
+            
+            for (i = 0; i < len; i++)
+            {
+                chart1.Series["nodes"].Points.AddXY
+                                (ListTest.list_coorx[i], ListTest.list_coory[i]);
+                chart1.Series["edges"].Points.AddXY
+                                (ListTest.list_coorx[i], ListTest.list_coory[i]);
+            }
+
+
+            chart1.Series["nodes"].ChartType = SeriesChartType.Point;
+            chart1.Series["nodes"].Color = Color.Red;
+
+            chart1.Series["edges"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["edges"].Color = Color.Blue;
+
+            len =len / 2;
+            double [,] Distance = new double[len, len];            
+
+            for (i = 0; i < len; i++)
+            {
+                double x = ListTest.list_coorx[i];
+                double y = ListTest.list_coorx[i];
+
+
+                for (int j = 0; j < len; j++)
+                {
+                    Distance[i,j] = Math.Sqrt(Math.Pow(x - ListTest.list_coorx[j], 2) + Math.Pow(y - ListTest.list_coory[j], 2));
+                }
+            }
+
+            int[] tour = new int[len];
+
+            init_gen(tour, len);
+
+
+            //foreach(int el in tour)richTextBox1.Text = richTextBox1.Text + len +  "\t" + el + "\n";
+
+            double cost = 0;
+
+            for (i = 0; i < len - 1; i++)
+            {
+                richTextBox1.Text = richTextBox1.Text + tour[i] + " to " + tour[i+1] + " -- " + Distance[tour[i], tour[i+1]] + "\n";
+            }
+
+            richTextBox1.Text = richTextBox1.Text + tour[len-1] + " to " + tour[0] + " -- " + Distance[tour[len-1], tour[0]] + "\n";
+            cost = Eval(Distance, tour, len);
+
+            textBox1.Text = textBox1.Text + cost;
+
+        }
+
+        private void infoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is a Travelling Salesman Problem(TSP) solver which is created using Artificial Immune System(AIS)", "Software information");
+        }
+
+        private void showCostToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
